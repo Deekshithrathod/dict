5db0220c75246f4d95f285a862d60534918d2b02
Deekshithrathod <68671324+Deekshithrathod@users.noreply.github.com>
2022-12-09 17:03:29 +0530

    added bin & commands to publish on npm

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 import * as readline from "node:readline/promises";
 import { stdin as input, stdout as output } from "node:process";
 import { createSpinner } from "nanospinner";
@@ -8,13 +10,9 @@ import data from "./data.js";
 const rl = readline.createInterface({ input, output });
 const word = await rl.question("word please -> ");
 
-const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
-
 const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
 async function getData() {
   const spinner = createSpinner("Beep boop beep boop...").start();
-  // await sleep();
-  // display(data);
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
